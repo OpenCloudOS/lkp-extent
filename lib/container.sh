488ee7145f34686210692e9ea35d67c1534798e5
BIGWJZ <1059537323@qq.com>
2022-09-20 18:28:05 +0800

    fix path

--- lib/container.sh
+++ lib/container.sh
@@ -6,6 +6,8 @@ do_container_test(){
     local container_type=$1
 
     shift 1
+
+    mkdir -p $ROOT_DIR/lkp_tests/dockerResults
     #${container_type}_create_image $DOCKER_INAME
     ${container_type}_create_container $DOCKER_INAME $VM_CNT
     #${container_type}_test_container $1 
